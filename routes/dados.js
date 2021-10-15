e8e90590902ff823038fefadc84e25e18c593a84
Edwars Rodriguez <edwarsrr@gmail.com>
2021-10-14 21:34:32 -0300

    testando POST

--- routes/dados.js
+++ routes/dados.js
@@ -1,33 +1,11 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const Dados = require("./../models/dados");
 const mongoose = require("mongoose")
-const dados = require("./../models/dados");
-const { Router } = require("express");
-const express = require('express')
-const router = express.Router()
 const Dado = require('../models/dados')
 
-router.post( '/', async (req, res, next) => {
-    req.dados = new Dados()
-    next()
-    let dados = new Dados({
-        nome : req.body.nome,
-        sobrenome : req.body.sobrenome,
-        cpf : req.body.cpf,
-        endereco : req.body.endereco,
-        plano : req.body.plano,  
-    })
-    try{
-        dados = await dados.save()
-        res.redirect(`/dados/${dados.id}`)
-    } catch(e){
-        console.log(e)
-    }
-
-}  )
 // Creating one
 router.post('/', async (req, res) => {
+  try{
   const dado = new Dado({
     nome: req.body.nome,    
     sobrenome: req.body.sobrenome,
@@ -36,61 +14,21 @@ router.post('/', async (req, res) => {
     plano: req.body.plano
     
   })
-
+  }catch (err) {
+    res.status(400).json({ message: err.message })
+  }
 router.delete('/:id', async (req, res) => {
     await Dado.findByIdAndDelete(req.params.id)
     res.redirect('/')
   })
 
-  router.get('/:id', async(req,res) =>{
-    const Dado = await Dado.findById(req.params.id)
-    res.render('dados/shows', (dado: dado)) //Não sei como corregi esse erro
-  })
-
-  router.get('/edit/:id', async(req, res) =>{ 
-    const Dado = await Dado.findById(req.params.id)
-    res.render('dados/edit',(dado: dado) )// mesmo erro
-  })
-
-  // para pedir por banco de dados
-
-  router.post('/', async(req, res, next)=>{
-    req.dado = newDado()
-
-    next()
-  }),salvar_editar('/new');
-
-  router.put('/:id', async(req,res,next) =>{
-    req.dado = await Dado.findbyId(req,params,id);
-    next();
-  },salvar_editar('edit'));
-  function salvar_editar(path){
-    return async(req,res)=> {
-      let dado = require.dado
-        dado.nome = req.body.nome,
-        dado.sobrenome = req.body.sobrenome,
-        dado.cpf = req.body.cpf,
-        dado.endereco = req.body.endereco,
-        dado.plano = req.body.plano,  
-      }
-    
-      try{
-        dados = await dados.save()
-        res.redirect(`/dados/${dados.id}`)
-    } catch(e){
-        console.log(e)
-    }
-    }
-  }
-
-
-  
   try {
     const newDado = await dado.save()
     res.status(201).json(newDado)
   } catch (err) {
     res.status(400).json({ message: err.message })
-  };
+  }
+})
 
-module.exports = router
+
 module.exports = router
--- server.js
+++ server.js
@@ -1,30 +1,21 @@
 const express = require('express');
 const app = express();
-const router = require("./routes/dados")
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const dotenv = require("dotenv");
 require('dotenv').config()
-
-const express = require('express')
-const app = express()
 const mongoose = require('mongoose')
 
-const port = 3000
-const mongoose = require('mongoose');
-require('dotenv').config()
-mongoose.connect(process.env.MONGO, {
-})
-
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: false}))
-app.get("/", (req,res) => {
-    res.send("Negocio Pocket")
-} )
 mongoose.connect(process.env.MONGO, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Conectado ao Banco de Dados'))
 
-app.use("/dados", router)
 app.use(express.json())
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false}))
 
 const dadosRouter = require('./routes/dados')
 app.use('/dados', dadosRouter)
@@ -33,7 +24,7 @@ app.get('/', (req, res) => {
     res.send("Negocio Pocket")
 })
 
-app.listen(port, ()=> {
-    console.log(`Rodando servidor pelo localhost $(3000)`)
-});
-app.listen(3000, () => console.log('Servidor Iniciado'))
+app.listen(8080, () => console.log('Servidor Iniciado')) 
+
+
+
